29d5c5f7b17773220f51391e44341406be7e8884
CubedProgrammer <47759847+CubedProgrammer@users.noreply.github.com>
2021-09-16 11:28:26 -0400

    Linux diskspace.c

---
+++ diskspace.c
@@ -0,0 +1,42 @@
+#include<fcntl.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<sys/statvfs.h>
+#include<unistd.h>
+#include<cpcdt_date_struct.h>
+int main(int argl, char *argv[])
+{
+    char cbuf[100];
+    char mdat[20000];
+    const char search[8] = "/dev/sd";
+    int mounts = open("/proc/self/mounts", O_RDONLY);
+    size_t msize = read(mounts, mdat, sizeof(mdat));
+    close(mounts);
+    mdat[msize] = '\0';
+    char *devstr = strstr(mdat, search);
+    char *mpstr, *spaceptr;
+    struct statvfs disk;
+    struct statvfs *pdisk = &disk;
+    unsigned long total, remaining, used;
+    cpcdt_date date = cpcdt_make_date(sec_since_epoch());
+    cpcdt_readable_date(cbuf, date);
+    FILE *file = fopen("diskspace.txt", "a");
+    fputs(cbuf, file);
+    fputc('\n', file);
+    while(devstr != NULL)
+    {
+        mpstr = strchr(devstr, ' ');
+        *mpstr++ = '\0';
+        spaceptr = strchr(mpstr, ' ');
+        *spaceptr = '\0';
+        statvfs(mpstr, pdisk);
+        total = disk.f_frsize * disk.f_blocks;
+        remaining = disk.f_frsize * disk.f_bfree;
+        used = total - remaining;
+        fprintf(file, "%s %s %lu %lu %lu\n", devstr, mpstr, used, remaining, total);
+        devstr = strstr(spaceptr + 1, search);
+    }
+    fclose(file);
+    return 0;
+}
